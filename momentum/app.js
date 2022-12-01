01ae34448b9066a89e5a753df1762b1d829f95a0
Ju eun <110323867+cobaltune@users.noreply.github.com>
2022-12-02 01:38:03 +0900

    (ing)

--- momentum/app.js
+++ momentum/app.js
@@ -1,3 +1,12 @@
-const title = document.querySelector('.hello h1');
+const h1 = document.querySelector('div.hello:first-child h1');
 
-console.log(title);
+function handleTitleClick() {
+  const clickedClass = 'clicked sexy-font';
+  if (h1.className === clickedClass) {
+    h1.className = '';
+  } else {
+    h1.className = clickedClass;
+  }
+}
+
+h1.addEventListener('click', handleTitleClick);
