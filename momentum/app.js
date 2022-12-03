b6f4d3c954c5c6ffa0ab3a250581ac2471ed2ce4
Ju eun <110323867+cobaltune@users.noreply.github.com>
2022-12-03 23:09:40 +0900

    ing

--- momentum/app.js
+++ momentum/app.js
@@ -1,7 +1,9 @@
-const h1 = document.querySelector('div.hello:first-child h1');
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
 
-function handleTitleClick() {
-  h1.classList.toggle('clicked');
+function onLoginSubmit() {
+  const username = loginInput.value;
+  console.log(username);
 }
 
-h1.addEventListener('click', handleTitleClick);
+loginForm.addEventListener('submit', onLoginSubmit);
