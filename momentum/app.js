18780439ce0604838e1cbce20cbb5117150c84c8
Ju eun <110323867+cobaltune@users.noreply.github.com>
2022-12-04 16:45:52 +0900

    ing

--- momentum/app.js
+++ momentum/app.js
@@ -1,16 +1,15 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
 
-const link = document.querySelector('a');
+const HIDDEN_CLASSNAME = 'hidden';
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
-}
-
-function handleLinkClick(event) {
-  event.preventDefault();
+  loginForm.classList.add('hidden');
+  const username = loginInput.value;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener('submit', onLoginSubmit);
-link.addEventListener('click', handleLinkClick);
