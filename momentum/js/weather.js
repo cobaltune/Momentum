6443832051f52b095d1fed4c9b120de2652ce4ae
Ju eun <110323867+cobaltune@users.noreply.github.com>
2022-12-10 18:09:16 +0900

    add

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -1,6 +1,17 @@
+const API_KEY = 'ed7e9b3bf111eecb1cdfecf73f79d605';
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
+  const lon = position.coords.longitude;
+  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector('#weather span:first-child');
+      const city = document.querySelector('#weather span:last-child');
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
