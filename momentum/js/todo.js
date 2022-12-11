583ca7428e1e483c07f0b2bcc843aa4cd855a6f1
Ju eun <110323867+cobaltune@users.noreply.github.com>
2022-12-11 23:48:40 +0900

    edit

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -23,7 +23,8 @@ function paintToDo(newTodo) {
   const span = document.createElement('span');
   span.innerText = newTodo.text;
   const button = document.createElement('button');
-  button.innerText = 'X';
+  button.classList.add('done');
+  button.innerText = 'DONE';
   button.addEventListener('click', deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
